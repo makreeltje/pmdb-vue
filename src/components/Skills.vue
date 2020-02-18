4de4de6af760da85d50afe8ec1a2c22301ba5dd7
Rick Meels <rick.meels@gmail.com>
2020-02-18 22:25:32 +0100

    Tutorial following

---
+++ src/components/Skills.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+      </ul>
+
+      <p>These are the skills that you possess.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      skills: [
+        { "skill" : "Vue.js" },
+        { "skill" : "Frontend Developer" }
+      ],
+      bgColor: 'yellow',
+      bgWidth: '100%',
+      bgHeight: '30px'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3e5252;
+  }
+
+  p {
+    text-align: center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgrey;
+  }
+</style>
